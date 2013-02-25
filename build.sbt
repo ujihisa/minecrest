ae36cfb06d89852216300325d077a268de8ec781
ujihisa <ujihisa@gmail.com>
2013-02-24 17:01:51 -0800

    trying to run the server.. refered this page http://www.scalatra.org/guides/deployment/standalone.html

--- build.sbt
+++ build.sbt
@@ -11,4 +11,7 @@ resolvers += "Akka Repo" at "http://repo.akka.io/repository"
 libraryDependencies ++= Seq(
 	"org.scalatra" %% "scalatra" % "2.2.0",
 	"org.scalatra" %% "scalatra-scalate" % "2.2.0",
-	"javax.servlet" % "servlet-api" % "2.5")
+	"javax.servlet" % "servlet-api" % "2.5",
+	"org.eclipse.jetty" % "jetty-server" % "9.0.0.RC0",
+	"org.eclipse.jetty" % "jetty-webapp" % "9.0.0.RC0",
+	"org.eclipse.jetty" % "jetty-servlet" % "9.0.0.RC0")
--- src/main/scala/com/github/ujihisa/minecrest/Main.scala
+++ src/main/scala/com/github/ujihisa/minecrest/Main.scala
@@ -2,6 +2,7 @@ package com.github.ujihisa.minecrest
 
 import org.scalatra.ScalatraFilter
 
+/*
 class Main extends ScalatraFilter {
 	sealed trait Entity
 	case class Player(name: String, health: Int) extends Entity
@@ -12,4 +13,28 @@ class Main extends ScalatraFilter {
 	get("/1/") {
 		mockOnlinePlayers.toString
 	}
+}
+ */
+
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.TemplateExample
+
+object Main {
+  def main(args: Array[String]) {
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+
+    val server = new Server(port)
+    val context = new WebAppContext()
+    context setContextPath "/"
+    context.setResourceBase("src/main/webapp")
+    context.addServlet(classOf[TemplateExample], "/*")
+    context.addServlet(classOf[DefaultServlet], "/")
+
+    server.setHandler(context)
+
+    server.start
+    server.join
+  }
 }
