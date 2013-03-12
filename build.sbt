082fc3e2c8ec13b0ea81cc59ea9d965303478545
ujihisa <ujihisa@gmail.com>
2013-03-11 21:47:09 -0700

    integrated with bukkit!

--- build.sbt
+++ build.sbt
@@ -10,9 +10,11 @@ resolvers ++= Seq(
 	//"Akka Repo" at "http://repo.akka.io/repository"
 	"spray repo" at "http://repo.spray.io",
 	"typesafe releases" at "http://repo.typesafe.com/typesafe/releases/",
-	"repo.codahale.com" at "http://repo.codahale.com")
+	"repo.codahale.com" at "http://repo.codahale.com",
+	"org.bukkit" at "http://repo.bukkit.org/service/local/repositories/snapshots/content/")
 
 libraryDependencies ++= Seq(
 	"com.typesafe.akka" % "akka-actor" % "2.0.4",
 	"com.codahale" % "jerkson_2.9.1" % "0.5.0",
-	"io.spray" % "spray-can" % "1.0-M7")
+	"io.spray" % "spray-can" % "1.0-M7",
+	"org.bukkit" % "bukkit" % "1.4.7-R1.1-SNAPSHOT")
--- src/main/scala/com/github/ujihisa/minecrest/Main.scala
+++ src/main/scala/com/github/ujihisa/minecrest/Main.scala
@@ -1,7 +1,7 @@
 package com.github.ujihisa.minecrest
 
 import spray.can.server.SprayCanHttpServerApp
-import akka.actor.{Props, Actor}
+import akka.actor.{Props, Actor, ActorSystem}
 
 class HttpHandler extends Actor {
 	import spray.http._
@@ -20,7 +20,8 @@ class HttpHandler extends Actor {
 			sender ! HttpResponse(entity = "OK")
 			
 		case HttpRequest(GET, "/api/v1/users/online.json", _, _, _) =>
-			sender ! jsonResponse(mockOnlinePlayers)
+			//sender ! jsonResponse(mockOnlinePlayers)
+			sender ! jsonResponse(org.bukkit.Bukkit.getOnlinePlayers.toList.map(_.getName))
 
 		case _: HttpRequest =>
 			sender ! HttpResponse(status = 404, entity = "Unknown resource!")
@@ -36,8 +37,34 @@ class HttpHandler extends Actor {
 		HttpResponse(entity = Json.generate(x))
 }
 
+/*
 object Main extends App with SprayCanHttpServerApp {
 	val handler = system.actorOf(Props[HttpHandler])
 
 	newHttpServer(handler) ! Bind(interface = "localhost", port = 12380)
+}
+ */
+
+import org.bukkit.plugin.java.JavaPlugin
+import com.typesafe.config.ConfigFactory
+class Main extends JavaPlugin with SprayCanHttpServerApp {
+	val config = ConfigFactory.parseFile(new java.io.File("/tmp/reference.conf")).resolve
+	//val classLoader = new java.net.URLClassLoader(Array(new java.net.URL("file:///home/share/tough-explosive/plugins/tough-explosive-0.1.0-SNAPSHOT-standalone.jar")))
+	//val classLoader = Thread.currentThread.getContextClassLoader
+	//val classLoader = ClassLoader.getSystemClassLoader
+	val previous = Thread.currentThread.getContextClassLoader
+	Thread.currentThread.setContextClassLoader(this.getClass().getClassLoader())
+	val system0 = ActorSystem("minecrest")//, config, classLoader)
+	val handler = system.actorOf(Props[HttpHandler])
+	newHttpServer(handler) ! Bind(interface = "localhost", port = 12380)
+	Thread.currentThread().setContextClassLoader(previous)
+	override lazy val system = system0
+
+	override def onEnable {
+		println('cool)
+	}
+
+	override def onDisable {
+		println('bye)
+	}
 }
